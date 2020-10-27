8577b55e759461f75fb0d219b07c76afe5b937ac
Dementev Slava <kursant344@gmail.com>
2020-10-27 17:35:48 +0300

    fix

--- app/src/main/java/com/dementev/interactingwiththelist/ListViewActivity.java
+++ app/src/main/java/com/dementev/interactingwiththelist/ListViewActivity.java
@@ -1,5 +1,6 @@
 package com.dementev.interactingwiththelist;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -8,6 +9,7 @@ import android.widget.SimpleAdapter;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,21 +17,40 @@ import java.util.List;
 import java.util.Map;
 
 public class ListViewActivity extends AppCompatActivity {
+    private SharedPreferences mySharedPref;
+    private static String NOTE_TEXT = "note_text";
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         ListView list = findViewById(R.id.list);
-
         List<Map<String, String>> values = prepareContent();
-
         BaseAdapter listContentAdapter = createAdapter(values);
-
         list.setAdapter(listContentAdapter);
+
+        mySharedPref = getSharedPreferences(getString(R.string.large_text), MODE_PRIVATE);
+        SharedPreferences.Editor myEditor = mySharedPref.edit();
+        String text = String.valueOf(getText(R.string.large_text));
+        myEditor.putString(NOTE_TEXT, text);
+        myEditor.apply();
+
+        list.setOnItemClickListener((parent, view, position, id) -> {
+            values.remove(position);
+            listContentAdapter.notifyDataSetChanged();
+        });
+
+        mSwipeRefreshLayout = findViewById(R.id.swiperefresh);
+        mSwipeRefreshLayout.setOnRefreshListener(() -> {
+            values.clear();
+            values.addAll(prepareContent());
+            mSwipeRefreshLayout.setRefreshing(false);
+            listContentAdapter.notifyDataSetChanged();
+        });
     }
 
     @NonNull
@@ -41,7 +62,7 @@ public class ListViewActivity extends AppCompatActivity {
 
     @NonNull
     private List<Map<String, String>> prepareContent() {
-        List<Map<String,String>> arrayList = new ArrayList<>();
+        List<Map<String, String>> arrayList = new ArrayList<>();
 
         String[] arrayContent = getString(R.string.large_text).split("\n\n");
 
